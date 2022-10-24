f3ef51d6ede78a87a1ff0d269b003be6e4a682fe
Thor <thor@joerger.dev>
2022-10-24 14:10:43 +0200

    Updated nextcloud custom version to 25.0.0

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -2,5 +2,5 @@
 
 # Update to wanted version here and in Dockerfile
 
-sudo docker build --tag thorjoerger/nextcloud:24.0.5 .
-sudo docker push thorjoerger/nextcloud:24.0.5
+sudo docker build --tag thorjoerger/nextcloud:25.0.0 .
+sudo docker push thorjoerger/nextcloud:25.0.0
