4cd94f8485129327fcb83ebbcc63ffe9767202a6
Thor <thor@joerger.dev>
2024-05-22 12:59:45 +0200

    Updated Nextcloud from 28.0.5 to 29.0.0

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -2,5 +2,5 @@
 
 # Update to wanted version here and in Dockerfile
 
-sudo docker build --tag thorjoerger/nextcloud:28.0.5 .
-sudo docker push thorjoerger/nextcloud:28.0.5
+sudo docker build --tag thorjoerger/nextcloud:29.0.0 .
+sudo docker push thorjoerger/nextcloud:29.0.0
