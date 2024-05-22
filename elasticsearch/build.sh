703190fea8abb0f763ba8c31e392e65b0f153885
Thor <thor@joerger.dev>
2024-05-22 12:55:18 +0200

    Updated Nextcloud from 28.0.4 to 28.0.5 and elasticsearch from 7.17.20 to 7.17.21

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -2,5 +2,5 @@
 
 # Update to wanted version here and in Dockerfile
 
-sudo docker build --tag thorjoerger/elasticsearch:7.17.20 .
-sudo docker push thorjoerger/elasticsearch:7.17.20
+sudo docker build --tag thorjoerger/elasticsearch:7.17.21 .
+sudo docker push thorjoerger/elasticsearch:7.17.21
