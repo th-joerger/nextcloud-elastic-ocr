8797b671e2c9626a0580d499b4a7adc621d37693
Thor <thor@joerger.dev>
2025-02-14 08:14:25 +0100

    Updated Nextcloud from 30.0.5 to 30.0.6

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -2,5 +2,5 @@
 
 # Update to wanted version here and in Dockerfile
 
-sudo docker build --tag thorjoerger/nextcloud:30.0.5 .
-sudo docker push thorjoerger/nextcloud:30.0.5
+sudo docker build --tag thorjoerger/nextcloud:30.0.6 .
+sudo docker push thorjoerger/nextcloud:30.0.6
